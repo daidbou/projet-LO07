238a17fc7dac94a81a277599c91a9fcb2d83d117
daidbou <89360632+daidbou@users.noreply.github.com>
2022-06-22 01:33:34 +0200

    Update viewChosen.php

--- app/view/individu/viewChosen.php
+++ app/view/individu/viewChosen.php
@@ -13,38 +13,47 @@
 ?>
     <ul>
         <?php
-                //données du tableau
-                echo(
-                    "
-                    <ul>
-                        <li>Né le $dateNaiss à $lieuNaiss .</li>
-                        <li>Décès le $dateDeces à $lieuDeces .</li>
-                    </ul>
+            //données du tableau
+            echo(
+                "
+                <ul>
+                    <li>Né le $dateNaiss à $lieuNaiss .</li>
+                    <li>Décès le $dateDeces à $lieuDeces .</li>
+                </ul>
 
-                    <h2>Parents</h2>
-                        
-                    <ul>\n<li>Père : <a href='router1.php?action=individuChosen&id=$idPere&idfam=$idfam'>".$nomPere." ".$prenomPere."</a></li>
-                        <li>Mère : <a href='router1.php?action=individuChosen&id=$idMere&idfam=$idfam'>".$nomMere." ".$prenomMere."</a></li>
-                                    
+                <h2>Parents</h2>
+                    
+                <ul>\n<li>Père : <a href='router1.php?action=individuChosen&id=$idPere&idfam=$idfam'>".$nomPere." ".$prenomPere."</a></li>
+                    <li>Mère : <a href='router1.php?action=individuChosen&id=$idMere&idfam=$idfam'>".$nomMere." ".$prenomMere."</a></li>
                                 
-                                        </ul>
-                        
-                    <h2>Unions et Enfants</h2>
-                        <ul>
+                            
+                                    </ul>
+                    
+                <h2>Unions et Enfants</h2>
+                    <ul>
 
-                "); 
-        foreach ($listeComp as $value) {
-            echo "<li>Union avec: <a href='router1.php?action=individuChosen&id={$value['id']}&idfam={$value['famille_id']}'>".$value['nom']." ".$value['prenom']."</a></li>";
-            echo("<ol>");
-            foreach($listeEnfant as $enfant){
-                if(($enfant['mere']==$value["id"] and $value["id"]!=0 and $value["sexe"]=="F") or ($enfant["pere"]==$value["id"]) and $value["id"]!=0and $value["sexe"]=="H"){
-                    echo("<li>Enfant : <a href='router1.php?action=individuChosen&id={$enfant['id']}&idfam={$enfant['famille_id']}'> {$enfant['nom']} {$enfant['prenom']} </a></li>");
+            "); 
+            if($listeComp!=NULL){ 
+                foreach ($listeComp as $value) {
+                    echo "<li>Union avec: <a href='router1.php?action=individuChosen&id={$value['id']}&idfam={$value['famille_id']}'>".$value['nom']." ".$value['prenom']."</a></li>";
+                    echo("<ol>");
+                    foreach($listeEnfant as $enfant){
+                        if($value['sexe']=='F'){
+                            if($value['id']==$enfant['mere'] and $enfant['pere']==$idind){
+                                echo("<li>Enfant : <a href='router1.php?action=individuChosen&id={$enfant['id']}&idfam={$enfant['famille_id']}'> {$enfant['nom']} {$enfant['prenom']} </a></li>");
+                            }
+                        }
+                        elseif($value['sexe']=='H'){
+                            if($value['id']==$enfant['pere'] and $enfant['mere']==$idind){
+                                echo("<li>Enfant : <a href='router1.php?action=individuChosen&id={$enfant['id']}&idfam={$enfant['famille_id']}'> {$enfant['nom']} {$enfant['prenom']} </a></li>");
+                            }
+                        }  
+                    }
+                    echo("</ol>");
+                    echo("<br>");
                 }
-            }
-            echo("</ol>");
-            echo("<br>");
-        }                 
-         echo("</ul>")
-            ?>          
+            }                 
+        echo("</ul>")
+        ?>          
 </div>
 <?php include $root . "/app/view/fragment/fragmentFooter.html" ?>
